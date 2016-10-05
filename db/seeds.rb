ba5c1663587726fa010e33026e718bae1a10c7ef
adrien pelegri <pelegriadrien@gmail.com>
2016-10-05 12:25:32 +0200

    flat card done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Flat.destroy_all
-flat = Flat.new(property_type: "Appartement" ,
+flat = Flat.new(property_type: "Appartement, Paris 15ième" ,
                 price: "650000",
-                room_number: "3 chambres",
-                square_meters: "80 m^2",
-                pieces_number: "3 pièces",
-                description: "3ème étage avec ascenseur Chauffage électrique Cave Commerces à proximité Commerces à proximité Ecoles Métro Blabla 1 SDB 2 WC 1 terrasse",
+                room_number: "3",
+                square_meters: "80",
+                pieces_number: "3",
+                description: "C'est un bien moderne, lumineux avec vue sur parc. Avec tout le confort requis pour une famille. ",
                 opinion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum est non eros accumsan rhoncus. Suspendisse in tellus vitae elit placerat iaculis sed vitae augue. Nunc eget gravida velit, quis aliquam tortor. Etiam fringilla, odio vitae elementum interdum, magna nulla malesuada magna, vel pellentesque erat turpis et augue. Vivamus sollicitudin ut neque vitae tempus. Vestibulum et diam urna. Vestibulum vel pulvinar est, eget cursus mi. Suspendisse at elit sed purus commodo gravida. Quisque cursus convallis nibh, eu faucibus est porta vel. Nulla quis tempor tellus, sed euismod justo. Morbi cursus facilisis erat, vel tristique nisi venenatis id. Cras dolor metus, volutpat vel eros pretium, convallis consequat lectus. Morbi id eros dignissim, facilisis metus a, egestas lacus. Donec varius lorem diam, quis vestibulum diam scelerisque vitae. Fusce in aliquet odio. Cras elit leo, porttitor nec pulvinar sagittis, ultricies sit amet justo.")
 flat.save!
