97926eadd9ab64ddddc83b7f546b96370baf60c0
farah falconi <farahfalconi@outlook.com>
2016-10-20 10:04:47 +0200

    change database to postgresql plus migration corrected

--- db/migrate/20161007095945_add_columnsenvironment_to_flats.rb
+++ db/migrate/20161007095945_add_columnsenvironment_to_flats.rb
@@ -2,7 +2,7 @@ class AddColumnsenvironmentToFlats < ActiveRecord::Migration[5.0]
   def change
     add_column :flats, :bus, :string
     add_column :flats, :metro, :string
-    add_column :flats, :school, :string
-    add_column :flats, :shop, :string
+    # add_column :flats, :school, :string
+    # add_column :flats, :shop, :string
   end
 end
