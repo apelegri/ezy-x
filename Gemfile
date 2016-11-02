f04a9a5d68a011b206db9e8948fdd334615f1923
adrien pelegri <pelegriadrien@gmail.com>
2016-11-02 14:52:13 +0100

    added mailer form

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "bootstrap-sass"
 gem "font-awesome-sass"
 gem "simple_form"
 gem "autoprefixer-rails"
+gem 'mail_form'
 source 'https://rails-assets.org' do
   gem 'rails-assets-plugin'
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,13 @@
 class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
+  attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
 
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
-      :to => "pelegriadrien@gmail.com",
+      :subject => "Contact Form",
+      :to => "contact@ezi-x.com",
       :from => %("#{name}" <#{email}>)
     }
   end
