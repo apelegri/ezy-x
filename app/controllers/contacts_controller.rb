3a77f74efbbad79398010c3ea157c8e3a0053218
adrien pelegri <pelegriadrien@gmail.com>
2016-11-02 17:04:55 +0100

    fixed contact design

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,5 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Votre message a bien été envoyé. Nous vous contactons le plus vite possible.'
-    else
-      flash.now[:error] = "Désole votre message n'est pas parti."
-      render :new
-    end
   end
 end
