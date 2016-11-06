311c3cce8bcbb45c5619a7bb968b891e434726e2
adrien pelegri <pelegriadrien@gmail.com>
2016-11-06 15:45:57 +0100

    erase migration error for prod

--- db/migrate/20161106075757_add_attachment_image_to_flats.rb
+++ db/migrate/20161106075757_add_attachment_image_to_flats.rb
@@ -1,11 +1,11 @@
-class AddAttachmentImageToFlats < ActiveRecord::Migration
-  def self.up
-    change_table :flats do |t|
-      t.attachment :image
-    end
-  end
+# class AddAttachmentImageToFlats < ActiveRecord::Migration
+#   def self.up
+#     change_table :flats do |t|
+#       t.attachment :image
+#     end
+#   end
 
-  def self.down
-    remove_attachment :flats, :image
-  end
-end
+#   def self.down
+#     remove_attachment :flats, :image
+#   end
+# end
