4d4a66fc6b966778d1e739924171fda50b075fcb
adrien pelegri <pelegriadrien@gmail.com>
2016-11-06 15:54:01 +0100

    erase migration error 3

--- db/migrate/20161106082110_remove_imagecolumns_from_flat.rb
+++
@@ -1,8 +0,0 @@
-class RemoveImagecolumnsFromFlat < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :flats, :image_file_name, :string
-    remove_column :flats, :image_content_type, :string
-    remove_column :flats, :image_file_size, :integer
-    remove_column :flats, :image_updated_at, :datetime
-  end
-end
