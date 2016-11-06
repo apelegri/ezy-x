94a01c4b1c886a7b77d4b6000ebd44147e53c825
adrien pelegri <pelegriadrien@gmail.com>
2016-11-06 21:26:39 +0100

    routes proeblems

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
-  match '/admin', to: 'admin/flats#index', via: :get
-  ActiveAdmin.routes(self)
-  devise_for :users
   root to: 'pages#home'
+
+  ActiveAdmin.routes(self)
+  match '/admin', to: 'admin/flats#index', via: :get
+  devise_for :users
   # get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   resources :flats, only: [:index, :show]
