777624a884ee32e30f404f53a8d09bf43b34169b
adrien pelegri <pelegriadrien@gmail.com>
2016-11-06 21:45:38 +0100

    added admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  match '/admin', to: 'admin/flats#index', via: :get
   ActiveAdmin.routes(self)
-  # match '/admin', to: 'admin/flats#index', via: :get
   devise_for :users
   # get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
