82d418b3b1272d706c9d0b8dfa56acaf2cdb8150
adrien pelegri <pelegriadrien@gmail.com>
2016-11-07 09:13:46 +0100

    debug admin in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   match '/admin', to: 'admin/flats#index', via: :get
-  ActiveAdmin.routes(self)
-  root to: 'pages#home'
   devise_for :users
+  root to: 'pages#home'
   # get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   resources :flats, only: [:index, :show]
   match '/contacts',     to: 'contacts#new',             via: 'get'
   resources "contacts", only: [:new, :create]
+  ActiveAdmin.routes(self)
 end
