1cb8bbe7bf8171b9c61a48fba1b6a80c05abd85f
adrien pelegri <pelegriadrien@gmail.com>
2016-11-07 09:32:49 +0100

    fix migration problem in prod

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   match '/admin', to: 'admin/flats#index', via: :get
   devise_for :users
   root to: 'pages#home'
-  # get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
   resources :flats, only: [:index, :show]
   match '/contacts',     to: 'contacts#new',             via: 'get'
--- db/migrate/20161106223435_remove_transportcolumn_to_flats.rb
+++ db/migrate/20161106223435_remove_transportcolumn_to_flats.rb
@@ -1,6 +1,6 @@
 class RemoveTransportcolumnToFlats < ActiveRecord::Migration[5.0]
   def change
-    remove_column :flats, :transport, :string
-    remove_column :flats, :transports, :string
+     add_column :flats, :transport, :string
+     add_column :flats, :transports, :string
   end
 end
