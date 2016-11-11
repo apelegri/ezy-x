8d6b62ea5e871b0c3addb7750f6aeff32d755ab3
adrien pelegri <pelegriadrien@gmail.com>
2016-11-11 12:06:30 +0100

    change target dns

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'ezi-x.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
@@ -97,6 +97,5 @@ ActionMailer::Base.smtp_settings = {
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer_url_options = { host: 'https://ezi-x.herokuapp.com/'}
-  config.action_mailer_url_options = { host: 'http://www.ezi-x.com/'}
   config.action_mailer.delivery_method = :smtp
 end
